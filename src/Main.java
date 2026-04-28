fd9db0da4a872b1d3b5b8cd7029546ab9c854f46
Justin <justinz121@nycstudents.net>
2026-04-28 10:17:29 -0400

    it wont work

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        for (int i = 0; i < 100; i++) {
-            System.out.print(util.randomGrade() + " ");
-            if (i % 20 == 0) {
-                System.out.println();
-            }
-        }
-        System.out.println();
-        System.out.println(util.allRooms());
-        */
 
         //Testing Department
         System.out.println();
