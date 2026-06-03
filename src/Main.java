29144b75d86a94031916b34c786d30f13b1783bb
Justin <justinz121@nycstudents.net>
2026-06-03 09:51:03 -0400

    Added create table statements to Main

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,83 @@ public class Main {
     public static void main(String[] args) {
 
 
+        System.out.println("CREATE TABLE Department (\n" +
+                "    DepartmentID int PRIMARY KEY,\n" +
+                "    DepartmentName varchar(255)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE Room (\n" +
+                "    RoomID int PRIMARY KEY,\n" +
+                "    RoomName varchar(255)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE Student (\n" +
+                "    StudentID int PRIMARY KEY,\n" +
+                "    Name varchar(255)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE AssignmentType (\n" +
+                "    AssignmentTypeID int PRIMARY KEY,\n" +
+                "    AssignmentTypeName varchar(255)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE CourseType (\n" +
+                "    CourseTypeID int PRIMARY KEY,\n" +
+                "    CourseTypeName varchar(255)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE Instructor (\n" +
+                "    InstructorID int PRIMARY KEY,\n" +
+                "    DepartmentID int,\n" +
+                "    Name varchar(255),\n" +
+                "    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE Course (\n" +
+                "    CourseID int PRIMARY KEY,\n" +
+                "    CourseTypeID int,\n" +
+                "    CourseName varchar(255),\n" +
+                "    FOREIGN KEY (CourseTypeID) REFERENCES CourseType(CourseTypeID)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE CourseOffering (\n" +
+                "    OfferingID int PRIMARY KEY,\n" +
+                "    InstructorID int,\n" +
+                "    CourseID int,\n" +
+                "    RoomID int,\n" +
+                "    Period int,\n" +
+                "    FOREIGN KEY (InstructorID) REFERENCES Instructor(InstructorID),\n" +
+                "    FOREIGN KEY (CourseID) REFERENCES Course(CourseID),\n" +
+                "    FOREIGN KEY (RoomID) REFERENCES Room(RoomID)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE Assignment (\n" +
+                "    AssignmentID int PRIMARY KEY,\n" +
+                "    OfferingID int,\n" +
+                "    AssignmentName varchar(255),\n" +
+                "    AssignmentTypeID int,\n" +
+                "    FOREIGN KEY (OfferingID) REFERENCES CourseOffering(OfferingID),\n" +
+                "    FOREIGN KEY (AssignmentTypeID) REFERENCES AssignmentType(AssignmentTypeID)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE Grade (\n" +
+                "    Grade int,\n" +
+                "    AssignmentID int,\n" +
+                "    StudentID int,\n" +
+                "    FOREIGN KEY (AssignmentID) REFERENCES Assignment(AssignmentID),\n" +
+                "    FOREIGN KEY (StudentID) REFERENCES Student(StudentID)\n" +
+                ");\n" +
+                "\n" +
+                "CREATE TABLE Roster (\n" +
+                "    StudentID int,\n" +
+                "    OfferingID int,\n" +
+                "    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),\n" +
+                "    FOREIGN KEY (OfferingID) REFERENCES CourseOffering(OfferingID)\n" +
+                ");\n" +
+                "\n" +
+                "\n"
+        );
+
         ArrayList<Room> roomObjects = new ArrayList<>();
         Room.generateRooms(roomObjects);
 
